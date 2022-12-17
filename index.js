a07c782628ec37f2ae5f9ca9248073bf9cced5ff
MohamedAmineSafi04 <mohammedameenalsafi@gmail.com>
2022-12-17 09:36:22 +0100

    Routing

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const fs = require("fs")
 const http = require("http")
+const url = require("url")
 
 const server = http.createServer((req, res) => {
-    res.end('Hello from the server'); // sending a response
+    const pathName = req.url
+
+    if (pathName === "/" || pathName === '/overview') {
+        res.end('This is the overview page'); // sending a response
+    } else if (pathName === "/product") {
+        res.end("This is the product");
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html' // pass header
+        }) // send 404 error
+        res.end("<h1>la9ad dalalta al-taree9</h1>")
+    }
 });
 
 server.listen(8000, '127.0.0.1', () => { //listen for incoming requests
