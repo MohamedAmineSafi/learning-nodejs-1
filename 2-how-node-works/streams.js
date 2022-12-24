3577c3316667b5363d3f4406097fa4cad83c464f
MohamedAmineSafi04 <mohammedameenalsafi@gmail.com>
2022-12-24 10:21:04 +0100

    Streams

--- 2-how-node-works/streams.js
+++ 2-how-node-works/streams.js
@@ -2,11 +2,23 @@ const fs = require('fs');
 const server = require('http').createServer();
 
 server.on('request', (req, res) => {
-  // Solution 1
-  fs.readFile('./2-how-node-works/test-file.txt', (err, data) => {
-    if (err) console.log(err);
-    res.end(data);
+  // Solution 2: Streams
+  const readable = fs.createReadStream('./2-how-node-works/test-file.txt');
+  readable.on('data', (chunk) => {
+    res.write(chunk);
   });
+  readable.on('end', () => {
+    res.end();
+  });
+  readable.on('error', (err) => {
+    console.log(err);
+    res.statusCode = 500;
+    res.end('File not found');
+  });
+
+  // Solution 3: Pipe
+  const readable2 = fs.createReadStream('./2-how-node-works/test-file.txt');
+  readable2.pipe(res);
 });
 
 server.listen(8000, 'localhost', () => {
