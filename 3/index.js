7e9881231f0bf05ebb5453f81bd1323d7049f3c3
MohamedAmineSafi04 <mohammedameenalsafi@gmail.com>
2022-12-25 10:06:45 +0100

    From Callback Hell to Promises

--- 3/index.js
+++ 3/index.js
@@ -7,14 +7,14 @@ fs.readFile(`${__dirname}/dog.txt`, 'utf-8', (err, data) => {
 
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-      if (err) {
-        return console.log(err.message);
-      }
+    .then((res) => {
       console.log(res.body.message);
 
       fs.writeFile(`${__dirname}/dogImage.txt`, res.body.message, (err) => {
         console.log('Dog Image Saved');
       });
+    })
+    .catch((err) => {
+      console.log(err.message);
     });
 });
